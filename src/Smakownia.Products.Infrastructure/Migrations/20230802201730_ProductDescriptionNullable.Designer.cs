77ff27d5fb77f8fa666f047d173fa0da36759844
aruzo <dawid12391111@gmail.com>
2023-08-02 22:23:21 +0200

    add migrations

--- src/Smakownia.Products.Infrastructure/Migrations/20230802201730_ProductDescriptionNullable.Designer.cs
+++ src/Smakownia.Products.Infrastructure/Migrations/20230802201730_ProductDescriptionNullable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Smakownia.Products.Infrastructure;
 
@@ -11,9 +12,11 @@ using Smakownia.Products.Infrastructure;
 namespace Smakownia.Products.Infrastructure.Migrations
 {
     [DbContext(typeof(ProductsContext))]
-    partial class ProductsContextModelSnapshot : ModelSnapshot
+    [Migration("20230802201730_ProductDescriptionNullable")]
+    partial class ProductDescriptionNullable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
