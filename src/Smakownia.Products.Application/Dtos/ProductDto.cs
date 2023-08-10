a604e602285515f112dda3ae0f9e978dd98ffa94
aruzo <dawid12391111@gmail.com>
2023-08-10 11:12:54 +0200

    create priceDto

--- src/Smakownia.Products.Application/Dtos/ProductDto.cs
+++ src/Smakownia.Products.Application/Dtos/ProductDto.cs
@@ -5,5 +5,5 @@ public class ProductDto
     public Guid CategoryId { get; set; }
     public string Name { get; set; } = string.Empty;
     public string? Description { get; set; }
-    public long Price { get; set; }
+    public PriceDto Price { get; set; } = new();
 }
