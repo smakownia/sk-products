ac81226b9e4a0814e4251df144a76b7a58e1f78c
aruzo <dawid12391111@gmail.com>
2023-08-13 10:36:41 +0200

    Update DeleteCategoryCommandHandler.cs

--- src/Smakownia.Products.Application/Commands/DeleteCategory/DeleteCategoryCommandHandler.cs
+++ src/Smakownia.Products.Application/Commands/DeleteCategory/DeleteCategoryCommandHandler.cs
@@ -1,4 +1,5 @@
 ﻿using MediatR;
+using Smakownia.Events;
 using Smakownia.Products.Domain.Abstractions;
 using Smakownia.Products.Domain.Repositories;
 
@@ -6,22 +7,35 @@ namespace Smakownia.Products.Application.Commands.DeleteCategory;
 
 public class DeleteCategoryCommandHandler : IRequestHandler<DeleteCategoryCommand, Unit>
 {
+    private readonly IEventBus _eventBus;
     private readonly IUnitOfWork _unitOfWork;
     private readonly ICategoriesRepository _categoriesRepository;
+    private readonly IProductsRepository _productsRepository;
 
-    public DeleteCategoryCommandHandler(IUnitOfWork unitOfWork, ICategoriesRepository categoriesRepository)
+    public DeleteCategoryCommandHandler(IEventBus eventBus,
+                                        IUnitOfWork unitOfWork,
+                                        ICategoriesRepository categoriesRepository,
+                                        IProductsRepository productsRepository)
     {
+        _eventBus = eventBus;
         _unitOfWork = unitOfWork;
         _categoriesRepository = categoriesRepository;
+        _productsRepository = productsRepository;
     }
 
     public async Task<Unit> Handle(DeleteCategoryCommand request, CancellationToken cancellationToken)
     {
         var category = await _categoriesRepository.GetByIdAsync(request.Id, cancellationToken);
+        var products = await _productsRepository.GetAllAsync((p) => p.CategoryId == category.Id, cancellationToken);
 
         _categoriesRepository.Remove(category);
 
         await _unitOfWork.CommitAsync(cancellationToken);
+
+        foreach (var product in products)
+        {
+            await _eventBus.PublishAsync(new ProductDeletedEvent(product.Id));
+        }
 
         return Unit.Value;
     }
