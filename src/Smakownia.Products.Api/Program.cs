ccbb6a21eedba4a74755309e95b87d847c0e064e
Dawid Ostrzyżek <64381826+aruzo1@users.noreply.github.com>
2023-08-14 16:50:35 +0200

    Feature/25 add role based authorization (#26)
    * update appsettings
    
    * update authorization

--- src/Smakownia.Products.Api/Program.cs
+++ src/Smakownia.Products.Api/Program.cs
@@ -1,18 +1,63 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Smakownia.Products.Api.Middlewares;
 using Smakownia.Products.Application;
 using Smakownia.Products.Domain.Abstractions;
 using Smakownia.Products.Domain.Repositories;
 using Smakownia.Products.Infrastructure;
 using Smakownia.Products.Infrastructure.Repositories;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication(opt => 
+{ 
+    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme; 
+    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme; 
+})
+    .AddJwtBearer(options => 
+    { 
+        options.TokenValidationParameters = new TokenValidationParameters
+        { 
+            ValidateIssuer = false, 
+            ValidateAudience = false, 
+            ValidateLifetime = true, 
+            ValidateIssuerSigningKey = true, 
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["SecretKey"])) 
+        }; 
+    });
 
 builder.Services.AddApplicationServices(builder.Configuration);
 builder.Services.AddInfrastructureServices(builder.Configuration);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(swagger =>
+{
+    swagger.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+    });
+    swagger.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            },
+            Array.Empty<string>()
+        }
+    });
+});
 
 var app = builder.Build();
 
@@ -26,6 +71,7 @@ app.UseMiddleware<GlobalExceptionMiddleware>();
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
